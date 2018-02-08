b8d2d25bd19d6e5fc7a2717e79c10cceacf24034
Alexander Urizar <urizaraj@gmail.com>
2018-02-08 21:23:27 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,5 +3,5 @@ def introduction(name)
 end
 
 def introduction_with_language(name, language)
-  puts "My name is #{name} and I speak #{langauge}"
+  puts "My name is #{name} and I speak #{language}"
 end
